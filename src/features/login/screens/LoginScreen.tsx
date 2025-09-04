5932f1cd91602951b3b557349fd5341434711b6c
Pankaj <pankaj@liquide.life>
2025-09-04 17:26:55 +0530

    Enhance AppView component with additional props for better style customization and create LoginScreen component

---
+++ src/features/login/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import AppView from '@/src/components/ui/AppView/AppView'
+import React from 'react'
+import { Text } from 'react-native'
+
+const LoginScreen = () => {
+  return (
+    <AppView>
+      <Text>LoginScreen</Text>
+    </AppView>
+  )
+}
+
+export default LoginScreen
